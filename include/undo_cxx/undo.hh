e0d561e11714fdeaa66f51eb563ffbb5fa4154d1
Hedzr Yeh <hedzrz@gmail.com>
2021-10-13 07:58:01 +0800

    adjust header files order

--- include/undo_cxx/undo.hh
+++ include/undo_cxx/undo.hh
@@ -1,3 +1,9 @@
+// undo_cxx Library
+// Copyright © 2021 Hedzr Yeh.
+//
+// This file is released under the terms of the MIT license.
+// Read /LICENSE for more information.
+
 //
 // Created by Hedzr Yeh on 2021/10/5.
 //
@@ -7,7 +13,18 @@
 
 #pragma once
 
+
+#include "undo_cxx-config.hh"
+#include "undo_cxx-version.hh"
+
+
 #include "undo-def.hh"
+
+#include "detail/undo-if.hh"
+
+#include "undo-common.hh"
+#include "undo-log.hh"
+
 #include "undo-unmgr.hh"
 
 #endif //UNDO_CXX_UNDO_HH
