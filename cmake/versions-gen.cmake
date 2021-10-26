55e029596b65406d3091d7cc15d16cf440ba97c4
hz <hedzrz@gmail.com>
2021-10-26 11:48:08 +0800

    fixed/improved version generator

--- cmake/versions-gen.cmake
+++ cmake/versions-gen.cmake
@@ -2,7 +2,7 @@
 
 # https://github.com/pmirshad/cmake-with-git-metadata/blob/master/CMakeLists.txt
 
-macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME ARCHIVE_NAME xVERSION_IN xCONFIG_BASE_IN)
+macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME CONFIG_H_NAME ARCHIVE_NAME xVERSION_IN xCONFIG_BASE_IN)
 
     if (DEFINED PROJ_NAME)
     else ()
@@ -17,6 +17,11 @@ macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME ARCHIVE_NAME xV
     if (DEFINED VERSION_H_NAME)
     else ()
         set(VERSION_H_NAME "${PROJ_NAME}-version.hh")
+    endif ()
+
+    if (DEFINED CONFIG_H_NAME)
+    else ()
+        set(CONFIG_H_NAME "${PROJ_NAME}-config.hh")
     endif ()
 
     if (DEFINED ARCHIVE_NAME)
@@ -35,6 +40,23 @@ macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME ARCHIVE_NAME xV
     message("Using version.in file: ${xVERSION_IN}, ARCHIVE_NAME = ${ARCHIVE_NAME}, PROJECT_MACRO_PREFIX = ${PROJECT_MACRO_PREFIX}")
 
     if (EXISTS "${CMAKE_SOURCE_DIR}/.git")
+
+        # git describe --tags --abbrev=0   # 0.1.0-dev
+        # git describe --tags              # 0.1.0-dev-93-g1416689
+        # git describe --abbrev=0          # to get the most recent annotated tag
+        execute_process(
+                COMMAND git describe --abbrev=0 --tags
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_LAST_TAG
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        execute_process(
+                COMMAND git describe --tags
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_LAST_TAG_LONG
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
         execute_process(
                 COMMAND git rev-parse --abbrev-ref HEAD
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -43,25 +65,36 @@ macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME ARCHIVE_NAME xV
         )
 
         execute_process(
-                COMMAND git log -1 --format=%h
+                COMMAND git rev-parse HEAD
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                 OUTPUT_VARIABLE GIT_COMMIT_HASH
                 OUTPUT_STRIP_TRAILING_WHITESPACE
         )
+
+        execute_process(
+                COMMAND git log -1 --format=%h
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_COMMIT_REV
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
     else ()
         set(GIT_BRANCH "")
+        set(GIT_LAST_TAG "")
+        set(GIT_LAST_TAG_LONG "")
         set(GIT_COMMIT_HASH "")
+        set(GIT_COMMIT_REV "")
     endif ()
 
-    # get_git_head_revision(GIT_REFSPEC GIT_SHA1)
-    string(SUBSTRING "${GIT_COMMIT_HASH}" 0 12 GIT_REV)
-    if (NOT GIT_COMMIT_HASH)
-        set(GIT_REV "0")
-    endif ()
+    #    # get_git_head_revision(GIT_REFSPEC GIT_SHA1)
+    #    string(SUBSTRING "${GIT_COMMIT_HASH}" 0 12 GIT_COMMIT_REV)
+    #    if (NOT GIT_COMMIT_HASH)
+    #        set(GIT_COMMIT_REV "0")
+    #    endif ()
 
-    message(STATUS "- Git current branch: ${GIT_BRANCH}")
-    message(STATUS "- Git commit hash:    ${GIT_COMMIT_HASH}")
-    message(STATUS "- Git rev:            ${GIT_REV}")
+    message(STATUS "- Git current branch:  ${GIT_BRANCH}")
+    message(STATUS "- Git last tag:        ${GIT_LAST_TAG}")
+    message(STATUS "- Git commit hash:     ${GIT_COMMIT_HASH}")
+    message(STATUS "- Git commit revision: ${GIT_COMMIT_REV}")
 
     if (NOT "${CMAKE_GENERATED_DIR}")
     else ()
@@ -79,12 +112,12 @@ macro(gen_versions PROJ_NAME PROJECT_MACRO_PREFIX VERSION_H_NAME ARCHIVE_NAME xV
     endif ()
 
     if (EXISTS ${xCONFIG_BASE_IN})
-        message(STATUS "Generating config-base.h from ${xCONFIG_BASE_IN} to ${CMAKE_GENERATED_DIR} - Version ${PROJECT_VERSION}...")
+        message(STATUS "Generating ${CONFIG_H_NAME} from ${xCONFIG_BASE_IN} to ${CMAKE_GENERATED_DIR} - Version ${PROJECT_VERSION}...")
         configure_file(
                 ${xCONFIG_BASE_IN}
-                ${CMAKE_GENERATED_DIR}/${PROJ_NAME}-config.hh
+                ${CMAKE_GENERATED_DIR}/${CONFIG_H_NAME}
         )
-        message(STATUS "Generated: ${CMAKE_GENERATED_DIR}/${PROJ_NAME}-config.hh")
+        message(STATUS "Generated: ${CMAKE_GENERATED_DIR}/${CONFIG_H_NAME}")
     endif ()
 
 
